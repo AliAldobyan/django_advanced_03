781c49b87565881331a45ce75ab3d6ec671cbc51
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-09-13 10:36:27 +0300

    some changes

--- stores/admin.py
+++ stores/admin.py
@@ -4,9 +4,9 @@ from .models import Store
 
 class StoreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
-    search_fields = ['name', 'description']
-    list_filter = ['added']
-    list_display_links = ['name']
-    list_editable = ['description']
+    search_fields = ('name', 'description',)
+    list_filter = ('added',)
+    list_display_links = ('name',)
+    list_editable = ('description',)
 
 admin.site.register(Store, StoreAdmin)
